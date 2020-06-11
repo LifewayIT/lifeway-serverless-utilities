a58b4aeb75bdc34ddc643678feab7aeef3230118
charliebthomas <charlie.thomas@lifeway.com>
2020-06-11 08:49:08 -0500

    Add get sub for httpApi authorizer

--- lib/request.ts
+++ lib/request.ts
@@ -1,11 +1,14 @@
 import { APIGatewayEvent } from 'aws-lambda';
 
+export const getOrigin = (event: APIGatewayEvent) => event?.headers && (event.headers.origin || event.headers.Origin);
 
-type Event = Partial<APIGatewayEvent>;
+export const parseBody = (event: APIGatewayEvent) =>
+  event?.body && JSON.parse(
+    event.isBase64Encoded
+      ? Buffer.from(event.body, 'base64').toString('utf8')
+      : event.body
+  );
 
-export const getOrigin = (event: Event) => event?.headers && (event.headers.origin || event.headers.Origin);
+  export const getUserId = (event: APIGatewayEvent) => event.requestContext?.authorizer?.principalId;
 
-export const parseBody = (event: Event) =>
-  event?.body && JSON.parse(event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString('utf8') : event.body);
-
-export const getUserId = (event: Event) => event?.requestContext?.authorizer?.principalId;
+  export const getSub = (event: APIGatewayEvent) => event.requestContext?.authorizer?.claims?.sub;
--- test/request.spec.ts
+++ test/request.spec.ts
@@ -1,18 +1,18 @@
-import { getOrigin, parseBody } from "../lib";
+import { getOrigin, parseBody, getSub, getUserId } from "../lib";
 
 test('event origin is returned from origin header', () => {
-  const event = { headers: { origin: 'localhost' } };
+  const event: any = { headers: { origin: 'localhost' } };
   expect(getOrigin(event)).toEqual(event.headers.origin);
 });
 
 test('event origin is returned from Origin header', () => {
-  const event = { headers: { Origin: 'localhost' } };
+  const event: any = { headers: { Origin: 'localhost' } };
   expect(getOrigin(event)).toEqual(event.headers.Origin);
 });
 
 test('event body is parsed into JSON object', () => {
   const body = '{ "test": "Standby. This is a test." }';
-  expect(parseBody({ body })).toEqual(JSON.parse(body));
+  expect(parseBody({ body } as any)).toEqual(JSON.parse(body));
 });
 
 test('event body is decoded if it is base64 encoded', () => {
@@ -21,5 +21,22 @@ test('event body is decoded if it is base64 encoded', () => {
     'gender': 'MALE',
     'id': '588F2E39-F869-43C0-9C39-9D3306EA98F8'
   };
-  expect(parseBody({ body, isBase64Encoded: true })).toEqual(decodedBody);
+  expect(parseBody({ body, isBase64Encoded: true } as any)).toEqual(decodedBody);
 });
+
+test('getSub', () => {
+  expect(getSub({ requestContext: {
+    authorizer: {
+      claims: {
+        sub: 'id'
+      }
+    }
+  }} as any)).toEqual('id');
+})
+test('getUserId', () => {
+  expect(getUserId({ requestContext: {
+    authorizer: {
+      principalId: 'id'
+    }
+  }} as any)).toEqual('id');
+})
