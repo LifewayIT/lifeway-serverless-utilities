61c316eef86fe027943380ca78ab9db5d7d70a2d
charliebthomas <charlie.thomas@lifeway.com>
2020-06-11 10:17:25 -0500

    build process

---
+++ scripts/versionCheck.js
@@ -0,0 +1,18 @@
+const versionExists = require('version-exists')
+
+const version = process.env.VERSION;
+
+versionExists('@lifeway/serverless-utilities', version)
+  .then(exists => {
+    if(exists) {
+      console.error('Package version already exists.');
+      process.exit(-1);
+    } else {
+      console.info(`${version} does not exist for package`);
+      process.exit(0);
+    }
+  })
+  .then(error => {
+    console.error(error);
+    process.exit(-1);
+  })
