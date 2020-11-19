064bfe461b24aa88c1d5d89562796ef75f0ead0e
Adam Herrington <adam.herrington@lifeway.com>
2020-11-19 13:55:39 -0500

    Convert to ts

--- lib/eventRouter.ts
+++ lib/eventRouter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 
 import logger from './logger';
 
@@ -8,20 +9,20 @@ const logPrefix = '[EVENT ROUTER]';
  * @param {*} options Router options.
  */
 export const route = (
-  routerRules,
+  routerRules: any,
   {
     source = 'KINESIS',
     gaurenteeOrder = false,
     enabled = true
   } = {}
 ) => {
-  const validRules = routerRules.every(route => 'handlers' in route && 'eventType' in route);
+  const validRules = routerRules.every((route: any) => 'handlers' in route && 'eventType' in route);
 
   if (!validRules) {
     throw new Error('Rules provided are not valid.');
   }
 
-  return async (event) => {
+  return async (event: any) => {
     logger.debug(logPrefix, 'AWS Lambda Event --', event);
     const lifewayEvents = getEventsFromSource(event, source);
 
@@ -34,10 +35,10 @@ export const route = (
 /**
  * @param {*} event AWS Lambda Kinesis Event (can contain many batched lifeway events.)
  */
-export const getEventsFromKinesis = (event) => {
+export const getEventsFromKinesis = (event: any) => {
   const events = event
     .Records
-    .map(record => Buffer.from(record.kinesis.data, 'base64').toString('utf8'))
+    .map((record: any) => Buffer.from(record.kinesis.data, 'base64').toString('utf8'))
     .filter(isJSON)
     .map(JSON.parse)
     .filter(validLifewayEvent);
@@ -48,7 +49,7 @@ export const getEventsFromKinesis = (event) => {
 /**
  * @param {*} event AWS Lambda SNS Event (will only ever contain a single lifeway event)
  */
-const getEventsFromSNS = (event) => {
+const getEventsFromSNS = (event: any) => {
   const lifewayEventString = event.Records[0].Sns.Message;
   const lifewayEvent = JSON.parse(lifewayEventString);
   if (validLifewayEvent(lifewayEvent)) {
@@ -62,7 +63,7 @@ const getEventsFromSNS = (event) => {
  * @param {*} event AWS Lambda Event (from kinesis or sns.... or whatever else we use in the future)
  * @param {*} source Configured source type.
  */
-const getEventsFromSource = (event, source) => {
+const getEventsFromSource = (event: any, source: any) => {
   switch (source.toUpperCase()) {
     case 'KINESIS':
       return getEventsFromKinesis(event);
@@ -80,18 +81,18 @@ const getEventsFromSource = (event, source) => {
  * @param {*} routerRules List of event router rules. Maps event types to handlers.
  * @param {*} gaurenteeOrder Boolean flag for gaurenteeing that the events are processed in order.
  */
-const processEvents = async (events, routerRules, gaurenteeOrder = false) => {
+const processEvents = async (events: any, routerRules: any, gaurenteeOrder = false) => {
   const process = gaurenteeOrder ? processInOrder : processInAnyOrder;
   return process(events, routerRules);
 };
 
 
-const bypass = async (events) => {
+const bypass = async (events: any) => {
   logger.info(logPrefix, 'Bypassing lifeway events --', events);
   return 'Bypassed lifeway event consumers.';
 };
 
-const processInOrder = async (events, routerRules) => {
+const processInOrder = async (events: any, routerRules: any) => {
   const results = [];
   for (const event of events) {
     const handlers = getHandlersForLifewayEvent(routerRules, event);
@@ -101,7 +102,7 @@ const processInOrder = async (events, routerRules) => {
       logger.warn(logPrefix, msg);
       results.push(msg);
     } else {
-      const promise = Promise.all(handlers.map(handler => handler(event).catch(handleError)));
+      const promise = Promise.all(handlers.map((handler: any) => handler(event).catch(handleError)));
       const res = await promise;
       logger.debug(logPrefix, `Event handlers processed event ${event.id} with result: [ ${res.join(' | ')} ]`);
       results.push(res);
@@ -110,27 +111,27 @@ const processInOrder = async (events, routerRules) => {
   return handleResults(results, events);
 };
 
-const processInAnyOrder = async (events, routerRules) => Promise.all(events
-  .map(event => {
+const processInAnyOrder = async (events: any, routerRules: any) => Promise.all(events
+  .map((event: any) => {
     logger.debug(logPrefix, 'Processing event --', event);
     const handlers = getHandlersForLifewayEvent(routerRules, event);
     if (handlers.length === 0) {
       logger.warn(logPrefix, 'No handlers defined for eventType --', event.eventType);
       return [];
     }
-    return Promise.all(handlers.map(handler => handler(event).catch(handleError)));
+    return Promise.all(handlers.map((handler: any) => handler(event).catch(handleError)));
   }))
-  .then(results => results.filter(list => list.length > 0))
+  .then(results => results.filter((list: any) => list.length > 0))
   .then(results => handleResults(results, events));
 
-const getHandlersForLifewayEvent = (routerRules, event) => {
-  const rule = routerRules.find(route => route.eventType === event.eventType);
+const getHandlersForLifewayEvent = (routerRules: any, event: any) => {
+  const rule = routerRules.find((route: any) => route.eventType === event.eventType);
   return rule && rule.handlers || [];
 };
 
-const handleResults = (results, events) => {
+const handleResults = (results: any, events: any) => {
   if (results.length === 0) {
-    const msg = `${logPrefix} No handlers triggered for events [ ${events.map(event => event.eventType).join(' | ')} ]`;
+    const msg = `${logPrefix} No handlers triggered for events [ ${events.map((event: any) => event.eventType).join(' | ')} ]`;
     logger.info(msg);
     return msg;
   }
@@ -140,7 +141,7 @@ const handleResults = (results, events) => {
   return msg;
 };
 
-const validLifewayEvent = (lifewayEvent) => {
+const validLifewayEvent = (lifewayEvent: any) => {
   const isValid =
     Object.prototype.hasOwnProperty.call(lifewayEvent, 'eventType') &&
     Object.prototype.hasOwnProperty.call(lifewayEvent, 'payload') &&
@@ -154,7 +155,7 @@ const validLifewayEvent = (lifewayEvent) => {
   return isValid;
 };
 
-const isJSON = (str) => {
+const isJSON = (str: any) => {
   try {
     JSON.parse(str);
     return true;
@@ -163,7 +164,7 @@ const isJSON = (str) => {
   }
 };
 
-const handleError = async (error) => {
+const handleError = async (error: any) => {
   if (error.retry) {
     logger.error(`<RETRYING> Recoverable error handling event --> ${JSON.stringify(error)}`);
     return Promise.reject(error);
--- test/eventRouter.spec.ts
+++ test/eventRouter.spec.ts
@@ -1,6 +1,6 @@
 import EventRouter from '../lib/eventRouter';
 
-const generateSNSEvent = (eventType, payload) => {
+const generateSNSEvent = (eventType: any, payload: any) => {
   return {
     Records: [
       {
@@ -12,9 +12,9 @@ const generateSNSEvent = (eventType, payload) => {
   };
 };
 
-const generateKinesisEvent = (events = []) => {
+const generateKinesisEvent = (events: any = []) => {
 
-  const records = events.map(evt => {
+  const records = events.map((evt: any) => {
     const jsonString = JSON.stringify({ id: Math.random().toString(36).substring(7), payload: evt.payload, version: 1, eventType: evt.eventType});
     return {
       kinesis: {
@@ -49,79 +49,73 @@ describe('eventRouter', () => {
     },
   ];
 
-  it('should route to tate handler -- sns', (done) => {
+  it('should route to tate handler -- sns', async () => {
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: true })(generateSNSEvent('TateTookLunch', { foo: 'bar'}))
+    return EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: true })(generateSNSEvent('TateTookLunch', { foo: 'bar'}))
       .then(res => {
         expect(tateHandler).toHaveBeenCalledTimes(1);
         expect(res).toContain('Completed processing 1 event(s): [ Yay ]');
-        done();
       });
   });
 
-  it('should not route when enabled option is false', (done) => {
+  it('should not route when enabled option is false', async () => {
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: true, enabled: false })(generateSNSEvent('TateTookLunch', { foo: 'bar'}))
+    return EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: true, enabled: false })(generateSNSEvent('TateTookLunch', { foo: 'bar'}))
       .then(res => {
 
         expect(tateHandler).toHaveBeenCalledTimes(0);
         expect(res).toEqual('Bypassed lifeway event consumers.');
-        done();
       });
   });
 
-  it('should route to charlie handler -- sns', (done) => {
+  it('should route to charlie handler -- sns', async () => {
     charlieHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('CharlieTookLunch', { foo: 'bar'}))
+    return EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('CharlieTookLunch', { foo: 'bar'}))
       .then(res => {
         expect(charlieHandler).toHaveBeenCalledTimes(1);
         expect(res).toContain('Completed processing 1 event(s): [ Yay ]');
-        done();
       });
   });
 
-  it('should route to all handlers -- sns', (done) => {
+  it('should route to all handlers -- sns', async () => {
     charlieHandler.mockResolvedValue('Yay');
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('TeamLunchHappened', { foo: 'bar'}))
+    return EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('TeamLunchHappened', { foo: 'bar'}))
       .then(res => {
         expect(charlieHandler).toHaveBeenCalledTimes(1);
         expect(res).toContain('Completed processing 1 event(s): [ Yay,Yay ]');
-        done();
       });
   });
 
-  it('should route to tate handler -- kinesis', (done) => {
+  it('should route to tate handler -- kinesis', async () => {
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'kinesis', gaurenteeOrder: true })(generateKinesisEvent([{eventType: 'TateTookLunch', payload: { foo: 'bar'}}]))
+    return EventRouter.route(exampleRules, { source: 'kinesis', gaurenteeOrder: true })(generateKinesisEvent([{eventType: 'TateTookLunch', payload: { foo: 'bar'}}]))
       .then(res => {
         expect(tateHandler).toHaveBeenCalledTimes(1);
         expect(res).toContain('Completed processing 1 event(s): [ Yay ]');
-        done();
       });
   });
 
-  it('should route to tate handler after ingoring an unkown event -- kinesis', (done) => {
+  it('should route to tate handler after ingoring an unkown event -- kinesis', async () => {
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'kinesis', gaurenteeOrder: true })(generateKinesisEvent([
+    return EventRouter.route(exampleRules, { source: 'kinesis', gaurenteeOrder: true })(generateKinesisEvent([
       { eventType: 'TateUpdatedSlackStatus', payload: { foo: 'bar' }},
       {eventType: 'TateTookLunch', payload: { foo: 'bar'}}
     ]))
       .then(res => {
         expect(tateHandler).toHaveBeenCalledTimes(1);
         expect(res).toContain('Completed processing 2 event(s): [ No handlers defined for eventType TateUpdatedSlackStatus | Yay ]');
-        done();
       });
   });
 
-  it('should route to all handlers -- kinesis', (done) => {
+  it('should route to all handlers -- kinesis', async () => {
     tateHandler.mockResolvedValue('Yay');
     charlieHandler.mockImplementation(() => {
       return new Promise((res) => {
         setTimeout(() => res('Yay'), Math.random() * 1000);
       });
     });
-    EventRouter.route(exampleRules)(generateKinesisEvent([
+    return EventRouter.route(exampleRules)(generateKinesisEvent([
       {eventType: 'TateTookLunch', payload: { foo: 'bar'}},
       {eventType: 'CharlieTookLunch', payload: { foo: 'bar'}},
       {eventType: 'TeamLunchHappened', payload: { foo: 'bar'}}
@@ -130,23 +124,21 @@ describe('eventRouter', () => {
         expect(tateHandler).toHaveBeenCalledTimes(2);
         expect(charlieHandler).toHaveBeenCalledTimes(2);
         expect(res).toContain('Completed processing 3 event(s): [ Yay | Yay | Yay,Yay ]');
-        done();
       });
   });
 
-  it('should handle unknown type', (done) => {
+  it('should handle unknown type', async () => {
     charlieHandler.mockResolvedValue('Yay');
     tateHandler.mockResolvedValue('Yay');
-    EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('NoSuchEvent', { you: 'suck'}))
+    return EventRouter.route(exampleRules, { source: 'SNS', gaurenteeOrder: false })(generateSNSEvent('NoSuchEvent', { you: 'suck'}))
       .then(res => {
         expect(charlieHandler).not.toHaveBeenCalled();
         expect(tateHandler).not.toHaveBeenCalled();
         expect(res).toContain('No handlers triggered for events [ NoSuchEvent ]');
-        done();
       });
   });
 
-  it('should get events from kinesis', () => {
+  it('should get events from kinesis', async () => {
     const events = EventRouter.getEventsFromKinesis(generateKinesisEvent([
       {eventType: 'TateTookLunch', payload: { foo: 'bar'}},
       {eventType: 'CharlieTookLunch', payload: { foo: 'bar'}},
